03a3bf8e27a31b1a0fbdcba6fa4f765455741f7d
Madeline Jones <brandkast@live.com>
2019-02-08 22:44:59 -0500

    Updates

---
+++ precache-manifest.5db1f5d8c91f1353c55a95f85aa50419.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "248c13b04a995f8c18dd",
+    "url": "/cyobeauty/static/css/main.f8ad9dbf.chunk.css"
+  },
+  {
+    "revision": "248c13b04a995f8c18dd",
+    "url": "/cyobeauty/static/js/main.248c13b0.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/cyobeauty/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "f4935ecf18fa7d421c33",
+    "url": "/cyobeauty/static/js/runtime~main.f4935ecf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cyobeauty/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "65cf96ce33259ee7ec71abd845fefdd9",
+    "url": "/cyobeauty/index.html"
+  }
+];
